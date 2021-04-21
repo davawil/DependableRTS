075878c15e3e1f510c06a7805d58fca41fbd766d
lucasros98 <47554985+lucasros98@users.noreply.github.com>
2021-04-21 20:58:08 +0200

    problem 1 solution

--- MusicPlayer.h
+++ MusicPlayer.h
@@ -1,35 +1,47 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#define SLAVE_MODE 1
-#define LEADER_MODE 0
-#define SECOND_SLAVE_MODE 2
-
 #include "TinyTimber.h"
 #include "canTinyTimber.h"
 #include "sciTinyTimber.h"
+
+#define TONES_IN_TUNE 32
+#define TONE_HALF_PERIOD 500
+#define TONE_GAP 70
+#define KEY_MAX 5
+#define KEY_MIN -5
+#define TEMPO_MAX 240
+#define TEMPO_MIN 60
+#define MAX_VOLUME 10
+#define MIN_VOLUME 1
+
+#define BPM_TO_MSEC(X) 60000/X		//amount of milli seconds per beat provided the bpm
+#define MSEC_TO_BPM(X) 60000/X		//beats per minute provided the number of milliseconds
+
 typedef struct{
 	Object super;
-	int mode;
 	int tempo;
-	Time gap;
 	int key;
-	int NextTone;			//next index in tone-array
-	Time ToneDeadline;
-	int alive;
-	int startPos;
-	int tempoBuf;
-	int beat;
-}MusicPlayer;
+	int index;
+	int enabled;
+} MusicPlayer;
 
-void playTone(MusicPlayer *self, int unused);
-void endTone(MusicPlayer *self, int unused);
-void setTempo(MusicPlayer *self, int tempo);
-void setKey(MusicPlayer *self, int key);
-void toggleMusic(MusicPlayer *self, int unused);
-void changeStartPos(MusicPlayer *self, int n);
-void startMusic(MusicPlayer *self, int unused);
-void stopMusic(MusicPlayer *self, int unused);
-void setTempoBuf(MusicPlayer *self, int tempo);
+int get_tempo(MusicPlayer *, int);
+void end_tone(MusicPlayer *, int);
+void play_tune(MusicPlayer *self, int unused);
+void end_tone(MusicPlayer *self, int unused);
+void start_player(MusicPlayer *self, int unused);
+void stop_player(MusicPlayer *self, int unused);
+int get_player_enabled(MusicPlayer *self, int unused);
+void set_player_enabled(MusicPlayer *self, int value);
+void set_key(MusicPlayer *self, int value);
+void inc_key(MusicPlayer *self, int unused);
+void dec_key(MusicPlayer *self, int unused);
+void set_tempo(MusicPlayer *self, int value);
+void inc_tempo(MusicPlayer *self, int unused);
+void dec_tempo(MusicPlayer *self, int unused);
+void set_index(MusicPlayer *self, int value);
+int get_key(MusicPlayer *self, int unused);
+int get_tempo(MusicPlayer *self, int unused);
 
 #endif
--- ToneGen.c
+++ ToneGen.c
@@ -3,13 +3,15 @@ Time hp = USEC(1072);
 extern Serial sci0;
 void wave(ToneGen *self, int halfperiod){
 	if(self->alive){
-		if(self->wave){
-			*DIG_AN_CONV = self->volume;
-			self->wave = 0;
-		}
-		else{
-			*DIG_AN_CONV = 0;
-			self->wave = 1;
+		if(self->muted == 0) {
+				if(self->wave){
+				*DIG_AN_CONV = self->volume;
+				self->wave = 0;
+			}
+			else{
+				*DIG_AN_CONV = 0;
+				self->wave = 1;
+			}
 		}
 		SEND(USEC(halfperiod), self->deadline, self, wave, halfperiod);
 	}
@@ -29,4 +31,12 @@ void lowerVol(ToneGen *self, int unused){
  
 void setAlive(ToneGen *self, int alive){
 	self->alive = alive;
+}
+
+int getMuted(ToneGen *self,int unused) {
+	return self->muted;
+}
+
+void setMuted(ToneGen *self,int value) {
+	self->muted = value;
 }
